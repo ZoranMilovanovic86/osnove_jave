649778c9b4b45b39d6865c788555806d79c13b13
Zoran Milovanovic <mr.zoranmilovanovic@gmail.com>
2022-05-13 20:35:43 +0200

    Ispravka typo greske

--- osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1.java
+++ osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1.java
@@ -4,7 +4,7 @@ public class Git_Zadatak1 {
     public static void main(String[] args) {
         System.out.println("git clone");
         System.out.println("git add");
-        System.out.println("git restore --stage");
+        System.out.println("git restore --staged");
         System.out.println("git commit -m");
         System.out.println("git push");
 
